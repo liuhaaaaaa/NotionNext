ffa9ae066b4a4ccd6ac1c6992bae45847541e05a
liuhaaaaaa <94795256+liuhaaaaaa@users.noreply.github.com>
2023-06-17 15:01:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的🍚人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
-  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
+  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:'',
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '76136591@qq.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
